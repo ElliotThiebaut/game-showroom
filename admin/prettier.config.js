ed6398e7f26bc7e2f93c207af200b0ec61ba32cb
ElliotThiebaut <elliotthiebaut1@gmail.com>
2023-03-20 13:54:31 -0400

    Init new version

--- admin/prettier.config.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-    semi: true,
-    singleQuote: false,
-    arrowParens: "avoid",
-    tabWidth: 4,
-    printWidth: 200,
-    trailingComma: "all",
-};
--- client/src/main.js
+++
@@ -1,8 +0,0 @@
-import { createApp } from "vue";
-import { createPinia } from "pinia";
-import App from "./App.vue";
-import "./index.css";
-import "video.js/dist/video-js.min.css";
-import router from "@/router";
-
-createApp(App).use(router).use(createPinia()).mount("#app");
